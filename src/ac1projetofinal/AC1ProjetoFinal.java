4f2b474ccd8c55af2ca356e0c403f54c70d55c21
Lucas Spizzica <78517910+lcs-spizzica@users.noreply.github.com>
2021-10-14 22:44:59 -0300

    Add files via upload

---
+++ src/ac1projetofinal/AC1ProjetoFinal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac1projetofinal;
+
+
+import UI_UX.UI_Loading;
+import UI_UX.UI_Login;
+
+
+
+/**
+ *
+ * @author LucasSpizzica
+ */
+public class AC1ProjetoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UI_Loading carregamento = new UI_Loading();
+        carregamento.setVisible(true);
+    }
+    
+}
